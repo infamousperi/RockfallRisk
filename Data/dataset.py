1660983a49c567348f87ac3bfdd8930bd660d33b
Perinpagumar Munnanathan <perinpagumar.munnanathan@students.fhnw.ch>
2023-12-11 16:41:39 +0100

    calculated timedelta when reading csv

--- Data/dataset.py
+++ Data/dataset.py
@@ -16,10 +16,18 @@ def _read_csv(filename):
     df['Kinetic Energy [kJ]'] = cal.calculate_kinetic_energy_kj(df['Mass [kg]'], df['Velocity [m/s]'])
     new_df = _convert_date(df)
 
-    return new_df
+    return _calculate_timedelta(new_df)
 
 
 def _convert_date(df):
     df['Date'] = pd.to_datetime(df['Date'], format='mixed').dt.strftime('%d/%m/%Y')
     df['Month'] = pd.to_datetime(df['Date'], format='%d/%m/%Y').dt.strftime('%m')
+    return df
+
+
+def _calculate_timedelta(df):
+    df['DateTime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], dayfirst=True)
+    df['TimeDiffHours'] = df['DateTime'].diff().dt.total_seconds() / 3600
+    df['TimeDiffHours'] = df['TimeDiffHours'].fillna(0)
+
     return df
