c7ef90e8845015eacc3bd5303b07a021d81a6f21
Perinpagumar Munnanathan <perinpagumar.munnanathan@students.fhnw.ch>
2023-12-20 23:28:06 +0100

    calculated overall_death_probability

--- Calculations/probability.py
+++ Calculations/probability.py
@@ -4,3 +4,15 @@ def calculate_probability(df):
     probability = count / len(df)
 
     return probability
+
+
+def calculate_yearly_probability(total_probability, period):
+    probability = 1 - (1 - total_probability)**(1/period)
+
+    return probability
+
+
+def calculate_overall_death_probability(yearly_probability):
+    probability = (0.367 + 4.389) * 1.6 * yearly_probability
+
+    return probability
